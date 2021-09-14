bee4d1c4522aa9314a69fde119a456a3dedab8a1
Kabilan Sakthivelu <kabilansakthi97@gmail.com>
2021-09-14 10:31:35 +0530

    Error fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import firebase from 'firebase/compat/app';
-import {app} from './firebase';
-import {db} from './firebase';
-import {auth} from './firebase';
-import {useAuthState} from 'react-firebase-hooks/auth';
 import SignIn from './Components/SignIn';
 import MainContent from './Components/MainContent';
-import Modal from 'react-modal'
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {auth} from './firebase';
+import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 function App() {
